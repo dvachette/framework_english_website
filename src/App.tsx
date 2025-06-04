9d746dbc6f843f2da55b88ed44f83c487bc2b3e8
legregz <gregggmar01@gmail.com>
2025-06-04 11:25:48 +0200

    rework content-pane

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,39 @@ function App() {
           <div id="header-pane">
             <h1>Welcome to a world of ethic computers</h1>
           </div>
-          <br />
-          <br />
-          <br />
 
           <div id="content-pane">
-            <p id="elem1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni fuga labore quisquam vitae ea porro alias placeat. Cupiditate quisquam in suscipit sunt, ad voluptatem nulla sequi eos similique voluptatum.
-            Et at quae iure praesentium dicta consequuntur sint commodi fuga tenetur exercitationem laborum veniam vel asperiores non alias deserunt porro quibusdam facilis ex, quo accusantium expedita vero eum! Molestias, fugiat!</p>
-            <img className="img-round" src="src/assets/Press-Assets-img02-3641916910.jpg" alt="IUT Image" />
-            <p id="elem2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni fuga labore quisquam vitae ea porro alias placeat. Cupiditate quisquam in suscipit sunt, ad voluptatem nulla sequi eos similique voluptatum.
-            Et at quae iure praesentium dicta consequuntur sint commodi fuga tenetur exercitationem laborum veniam vel asperiores non alias deserunt porro quibusdam facilis ex, quo accusantium expedita vero eum! Molestias, fugiat!</p>
-            <p id="elem3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni fuga labore quisquam vitae ea porro alias placeat. Cupiditate quisquam in suscipit sunt, ad voluptatem nulla sequi eos similique voluptatum.
-            Et at quae iure praesentium dicta consequuntur sint commodi fuga tenetur exercitationem laborum veniam vel asperiores non alias deserunt porro quibusdam facilis ex, quo accusantium expedita vero eum! Molestias, fugiat!</p>
+	          <div id="social" className="item">
+		          <p id="elem1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni fuga labore quisquam vitae ea porro alias placeat. Cupiditate quisquam in suscipit sunt, ad voluptatem nulla sequi eos similique voluptatum.
+		          Et at quae iure praesentium dicta consequuntur sint commodi fuga tenetur exercitationem laborum veniam vel asperiores non alias deserunt porro quibusdam facilis ex, quo accusantium expedita vero eum! Molestias, fugiat!</p>
+		          <img className="img-round" src="src/assets/Press-Assets-img02-3641916910.jpg" alt="IUT Image" />
+	          </div>
+	          <div id="support" className="item">
+		          <p id="elem1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni fuga labore quisquam vitae ea porro alias placeat. Cupiditate quisquam in suscipit sunt, ad voluptatem nulla sequi eos similique voluptatum.
+		          Et at quae iure praesentium dicta consequuntur sint commodi fuga tenetur exercitationem laborum veniam vel asperiores non alias deserunt porro quibusdam facilis ex, quo accusantium expedita vero eum! Molestias, fugiat!</p>
+		          <img className="img-round" src="src/assets/Press-Assets-img02-3641916910.jpg" alt="IUT Image" />
+	          </div>
+	          <div id="action" className="item">
+		          <p id="elem1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni fuga labore quisquam vitae ea porro alias placeat. Cupiditate quisquam in suscipit sunt, ad voluptatem nulla sequi eos similique voluptatum.
+		          Et at quae iure praesentium dicta consequuntur sint commodi fuga tenetur exercitationem laborum veniam vel asperiores non alias deserunt porro quibusdam facilis ex, quo accusantium expedita vero eum! Molestias, fugiat!</p>
+		          <img className="img-round" src="src/assets/Press-Assets-img02-3641916910.jpg" alt="IUT Image" />
+	          </div>
+	          <div id="signal" className="item">
+		          <p id="elem1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni fuga labore quisquam vitae ea porro alias placeat. Cupiditate quisquam in suscipit sunt, ad voluptatem nulla sequi eos similique voluptatum.
+		          Et at quae iure praesentium dicta consequuntur sint commodi fuga tenetur exercitationem laborum veniam vel asperiores non alias deserunt porro quibusdam facilis ex, quo accusantium expedita vero eum! Molestias, fugiat!</p>
+		          <img className="img-round" src="src/assets/Press-Assets-img02-3641916910.jpg" alt="IUT Image" />
+	          </div>
+	          <div id="stories" className="item">
+		          <p id="elem1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni fuga labore quisquam vitae ea porro alias placeat. Cupiditate quisquam in suscipit sunt, ad voluptatem nulla sequi eos similique voluptatum.
+		          Et at quae iure praesentium dicta consequuntur sint commodi fuga tenetur exercitationem laborum veniam vel asperiores non alias deserunt porro quibusdam facilis ex, quo accusantium expedita vero eum! Molestias, fugiat!</p>
+		          <img className="img-round" src="src/assets/Press-Assets-img02-3641916910.jpg" alt="IUT Image" />
+	          </div>
           </div>
         </main>
         <aside className="sidebar">
           <Button id="bt-1" label="Click Me" onclick={() => scrollToElement("header-pane")} iconPath="/src/assets/home.png" />
-          <Button id="bt-2" label="Click Me" onclick={() => scrollToElement("elem2")} iconPath="/src/assets/home.png" />
-          <Button id="bt-3" label="Click Me" onclick={() => scrollToElement("elem3")} iconPath="/src/assets/home.png" />
+          <Button id="bt-2" label="Click Me" onclick={() => scrollToElement("social")} iconPath="/src/assets/home.png" />
+          <Button id="bt-3" label="Click Me" onclick={() => scrollToElement("support")} iconPath="/src/assets/home.png" />
         </aside>
       </div>
   );
