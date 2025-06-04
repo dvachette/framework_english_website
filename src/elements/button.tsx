7bd40592b454df58e6275373c210c5f58a6fb621
Donatien Vachette <donatien.vachette@hotmail.com>
2025-06-04 10:58:41 +0200

    pour push

--- src/elements/button.tsx
+++ src/elements/button.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 interface ButtonProps {
     label: string;
     onclick: () => void;
-    iconPath:string
+    iconPath:string,
+    id?: string;
 }
-function Button({ label, onclick, iconPath }: ButtonProps) {
+function Button({ label, onclick, iconPath, id}: ButtonProps) {
     const [isHovered, setIsHovered] = useState(false);
     const handleMouseEnter = () => {
         setIsHovered(true);
@@ -14,12 +15,11 @@ function Button({ label, onclick, iconPath }: ButtonProps) {
         setIsHovered(false);
     };
     return (
-        <button onClick={onclick} className="Button" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <button id={id} onClick={onclick} className="Button" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             {isHovered ? <p> {label} </p> : null}
             <img src={iconPath} alt="icon" className="ButtonIcon" /> 
         </button>
     );
 }
-
 
 export default Button;
